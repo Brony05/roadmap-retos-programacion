bcc5ffeed2e054a40404a3df0becd4a4b38aafdf
Brony05 <brony693@gmail.com>
2024-06-11 21:44:58 -0600

    Create Brony05.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/Brony05.cs
@@ -0,0 +1,26 @@
+//https://dotnet.microsoft.com/es-es/languages/csharp
+
+// comentario en una sola linea
+
+/*
+comentario 
+entre 
+lineas 
+*/
+
+class hola{
+      public static void Main(String[]args){
+          int=10;
+          const String NOMBRE="Cristian";//no se puede modificar ni asignar un nuevo valor eso es una (Constante)
+          int x = 50;//numeros enteros 
+          bool= true;//verdad 
+          bool= false;// o falso
+          char="Hola12$%";//almacena caracteres desde letras, numeros y o simbolos 
+          float=31.1416;//numeros deciamales 
+          string nombre="Maria";// cadena de caracteres
+          double distancia=152.120;///numeros decimales mas precisos
+          Console.Writline("Hola, C#");
+        
+      }
+}
+
